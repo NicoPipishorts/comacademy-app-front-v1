b2cc8c50c7eee1657d62b6ef536b3ca6596e579b
NicoPipishorts <87517338+NicoPipishorts@users.noreply.github.com>
2024-07-08 23:07:26 +0200

    Finished Layout of Metiers

--- constants/fontsizes.js
+++ constants/fontsizes.js
@@ -5,5 +5,6 @@ export const FontSizeH3 = 18;
 export const FontSizeH4 = 16;
 export const FontSizeTabbar = 11;
 export const FontSize12 = 12;
+export const FontSize16 = 16;
 export const FontSize20 = 20;
 export const FontSizeAvaterText = 22;
