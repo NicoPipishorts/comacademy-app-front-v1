550bece4ad5a8fa39995839c906cd22dc3b60b94
NicoPipishorts <87517338+NicoPipishorts@users.noreply.github.com>
2024-07-12 12:07:24 +0200

    Started Game Layout

--- app/(tabs)/le_jeu.jsx
+++ app/(tabs)/le_jeu.jsx
@@ -1,12 +1,80 @@
+import { primaryBackground } from "@/constants/colors";
 import React from "react";
-import { Text, View } from "react-native";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import FloatingTabBar from "../../components/FloatingTabBar";
+import ScreenHeaders from "../../components/ScreenHeaders";
+
+// Assets
+import { FontSizeScreenTitles } from "@/constants/fontsizes";
+import PlayButton from "../../assets/imgs/BigPlayButton.png";
 
 const LeJeu = () => {
 	return (
-		<View>
-			<Text>Le Jeu</Text>
+		<View style={styles.wrapper}>
+			<ScreenHeaders content='Le Jeu' />
+
+			<View style={styles.wrapperCenter}>
+				<View>
+					<Text style={styles.centerTitle}>A toi de jouer !</Text>
+				</View>
+				<View style={styles.playButtonContainer}>
+					<TouchableOpacity>
+						<Image
+							source={PlayButton}
+							resizeMode='contain'
+							style={styles.playButton}
+						/>
+					</TouchableOpacity>
+				</View>
+
+				<View style={styles.floatingTabbarContainer}>
+					<FloatingTabBar />
+				</View>
+			</View>
 		</View>
 	);
 };
+
+const styles = StyleSheet.create({
+	wrapper: {
+		flex: 1,
+		padding: 30,
+		paddingTop: 100,
+		backgroundColor: primaryBackground,
+	},
+	wrapperCenter: {
+		flex: 1,
+		width: "100%",
+		alignItems: "center",
+		justifyContent: "center",
+		marginBottom: 120,
+	},
+	centerTitle: {
+		fontSize: FontSizeScreenTitles,
+		fontWeight: "bold",
+		marginBottom: 30,
+	},
+	playButtonContainer: {
+		width: "100%",
+		alignItems: "center",
+	},
+	playButton: {
+		width: "60%",
+		height: undefined,
+		aspectRatio: 1,
+	},
+	floatingTabbarContainer: {
+		backgroundColor: "transparent",
+		flexDirection: "row",
+		justifyContent: "center",
+		alignItems: "center",
+		position: "absolute",
+		left: 0,
+		right: 0,
+		bottom: 0,
+		elevation: 5,
+		zIndex: 1,
+	},
+});
 
 export default LeJeu;
