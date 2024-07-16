b7681157a819c6496d0b5b4055a8ffe0808bef91
NicoPipishorts <87517338+NicoPipishorts@users.noreply.github.com>
2024-07-16 14:52:18 +0200

    removed react-native-dotenv

--- app/(tabs)/leJeu/jeu.tsx
+++ app/(tabs)/leJeu/jeu.tsx
@@ -5,10 +5,14 @@ import { useNavigation } from "expo-router";
 import React from "react";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { NavigationType } from ".";
-// Assets
 
 const Jeu = () => {
 	const navigation = useNavigation<NavigationType>();
+	const cards = [1, 2, 3, 4, 5];
+
+	const renderCard = (cardIndex: React.Key | null | undefined) => {
+		return <Card key={cardIndex} />;
+	};
 
 	const handlePress = () => {
 		navigation.navigate("index");
@@ -16,7 +20,20 @@ const Jeu = () => {
 
 	return (
 		<View style={styles.wrapper}>
-			<Card />
+			<View style={styles.container}>
+				<Card />
+				{/* <Swiper
+					cards={cards}
+					renderCard={renderCard}
+					onSwiped={(cardIndex) => console.log("Card swiped:", cardIndex)}
+					onSwipedLeft={(cardIndex) => console.log("Swiped left:", cardIndex)}
+					onSwipedRight={(cardIndex) => console.log("Swiped right:", cardIndex)}
+					infinite // Loop through cards
+					backgroundColor={"transparent"}
+					cardVerticalMargin={80}
+					stackSize={3} // Number of cards shown in stack
+				/> */}
+			</View>
 			<View style={styles.containerBackButton}>
 				<TouchableOpacity onPress={handlePress} style={styles.backButton}>
 					<Text style={styles.textBackButton}>Quitter</Text>
@@ -34,6 +51,10 @@ const styles = StyleSheet.create({
 		backgroundColor: primaryBackground,
 		justifyContent: "flex-start",
 		alignItems: "center",
+	},
+	container: {
+		flex: 1,
+		backgroundColor: "#f2f2f2",
 	},
 	containerBackButton: {
 		position: "absolute",
