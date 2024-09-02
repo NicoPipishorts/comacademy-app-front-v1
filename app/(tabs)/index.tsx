3917fa04d8146040cafda51bb9c4d0096f1734e4
NicoPipishorts <87517338+NicoPipishorts@users.noreply.github.com>
2024-09-02 23:37:09 +0200

    updated Link for MyStats Card

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -79,7 +79,9 @@ const HomeScreen = () => {
 								/>
 							</View>
 						</TouchableOpacity>
-						<TouchableOpacity style={styles.cardsButton}>
+						<TouchableOpacity
+							style={styles.cardsButton}
+							onPress={() => navigation.navigate("user")}>
 							<View style={styles.imageContainer}>
 								<Image
 									source={mesStats}
