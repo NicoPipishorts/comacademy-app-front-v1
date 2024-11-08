6b10c126675e53c54aba43868d4d31f72fe89e21
NicoPipishorts <87517338+NicoPipishorts@users.noreply.github.com>
2024-11-08 11:41:21 +0100

    Updated EAS (#77)
    * Updated EAS
    
    * notification provider done.
    
    * Removed Scheduled Notifications.

--- app/(tabs)/secrets/_layout.tsx
+++ app/(tabs)/secrets/_layout.tsx
@@ -1,5 +1,5 @@
 import { SecretsProvider } from "@/context/contextSecrets";
-import { GameProvider } from "@/providers/gameDataContext";
+import { GameProvider } from "@/providers/GameDataContext";
 import { Stack } from "expo-router";
 import React from "react";
 
