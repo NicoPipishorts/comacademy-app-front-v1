c76170550be9e77c4efae2d8a9d22a681d01cb25
NicoPipishorts <87517338+NicoPipishorts@users.noreply.github.com>
2025-01-11 00:09:58 +0100

    fixed package.json libs

--- app/(tabs)/playlists/playlistList.tsx
+++ app/(tabs)/playlists/playlistList.tsx
@@ -255,7 +255,7 @@ const styles = StyleSheet.create({
 	},
 	contentContainerEmpty: {
 		flexGrow: 1,
-		minwidth: "84%",
+		minWidth: "84%",
 		justifyContent: "center",
 		alignItems: "center",
 	},
