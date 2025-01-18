7126ae86b0c21fe4db26ecc42d900e1b84ec8766
NicoPipishorts <87517338+NicoPipishorts@users.noreply.github.com>
2025-01-18 23:45:51 +0100

    Local Notifications Done.

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -5,41 +5,31 @@ import {
 	TabBarVisibilityProvider,
 	useTabBarVisibility,
 } from "@/context/TabBarVisibilityContext";
-import {
-	PushNotificationContext,
-	PushNotificationProvider,
-} from "@/providers/PushNotificationProvider";
 import Register from "@/screens/Register/Register";
 import * as Notifications from "expo-notifications";
 import { Tabs } from "expo-router";
-import { useContext, useEffect } from "react";
+import { useEffect } from "react";
 import LoginScreen from "../../screens/Sign-in";
-
-Notifications.setNotificationHandler({
-	handleNotification: async () => ({
-		shouldShowAlert: true,
-		shouldPlaySound: false,
-		shouldSetBadge: false,
-	}),
-});
+import NotificationScheduler from "../services/notifications/LocalNotifications";
 
 const CustomTabBar: React.FC<any> = (props) => {
 	const { isTabBarVisible } = useTabBarVisibility();
-
-	const { notification } = useContext(PushNotificationContext);
+	useEffect(() => {
+		Notifications.setNotificationHandler({
+			handleNotification: async () => ({
+				shouldShowAlert: true,
+				shouldPlaySound: true,
+				shouldSetBadge: false,
+			}),
+		});
+	}, []);
 
 	// Display notification alert if one is received
-	useEffect(() => {
-		//TODO NOTIFICATION : add notification details
-		// if (notification) {
-		// 	return null;
-		// }
-	}, [notification]);
 
 	if (!isTabBarVisible) return null;
 
 	return <TabBar {...props} />;
-};
+}; // Import the scheduler
 
 const _layout: React.FC = () => {
 	const { isAuthenticated, isRegistering } = useAuth();
@@ -53,107 +43,107 @@ const _layout: React.FC = () => {
 	}
 
 	return (
-		<PushNotificationProvider>
-			<TabBarVisibilityProvider>
-				<Tabs tabBar={(props) => <CustomTabBar {...props} />}>
-					<Tabs.Screen
-						name='index'
-						options={{
-							tabBarLabel: "index",
-							headerShown: false,
-						}}
-					/>
-					<Tabs.Screen
-						name='activity'
-						options={{
-							tabBarLabel: "Accueil",
-							headerShown: false,
-							lazy: true,
-						}}
-					/>
-					<Tabs.Screen
-						name='leJeu'
-						options={{
-							tabBarLabel: "Le Jeu",
-							headerShown: false,
-							lazy: true,
-						}}
-					/>
-					<Tabs.Screen
-						name='feed'
-						options={{
-							tabBarLabel: "Feed",
-							headerShown: false,
-							unmountOnBlur: true,
-						}}
-					/>
-					<Tabs.Screen
-						name='playlists'
-						options={{
-							tabBarLabel: "Playlists",
-							headerShown: false,
-							unmountOnBlur: true,
-						}}
-					/>
-					<Tabs.Screen
-						name='dico'
-						options={{
-							tabBarLabel: "Dico",
-							headerShown: false,
-							unmountOnBlur: true,
-						}}
-					/>
-					<Tabs.Screen
-						name='metiers'
-						options={{
-							tabBarLabel: "Metiers",
-							headerShown: false,
-							unmountOnBlur: true,
-						}}
-					/>
-					<Tabs.Screen
-						name='user'
-						options={{
-							tabBarLabel: "User",
-							headerShown: false,
-							href: null,
-						}}
-					/>
-					<Tabs.Screen
-						name='lesCitations'
-						options={{
-							tabBarLabel: "Les Citations",
-							headerShown: false,
-							unmountOnBlur: true,
-						}}
-					/>
-					<Tabs.Screen
-						name='secrets'
-						options={{
-							tabBarLabel: "3 Secrets",
-							headerShown: false,
-							href: null,
-						}}
-					/>
-					<Tabs.Screen
-						name='commandements'
-						options={{
-							tabBarLabel: "10 Commandements",
-							headerShown: false,
-							href: null,
-						}}
-					/>
-					<Tabs.Screen
-						name='petitesHistoires'
-						options={{
-							tabBarLabel: "Les Petites Histoires",
-							headerShown: false,
-							href: null,
-						}}
-					/>
-				</Tabs>
-			</TabBarVisibilityProvider>
-		</PushNotificationProvider>
+		<TabBarVisibilityProvider>
+			{/* Add the notification scheduler */}
+			<NotificationScheduler />
+			<Tabs tabBar={(props) => <CustomTabBar {...props} />}>
+				<Tabs.Screen
+					name='index'
+					options={{
+						tabBarLabel: "index",
+						headerShown: false,
+					}}
+				/>
+				<Tabs.Screen
+					name='activity'
+					options={{
+						tabBarLabel: "Accueil",
+						headerShown: false,
+						lazy: true,
+					}}
+				/>
+				<Tabs.Screen
+					name='leJeu'
+					options={{
+						tabBarLabel: "Le Jeu",
+						headerShown: false,
+						lazy: true,
+					}}
+				/>
+				<Tabs.Screen
+					name='feed'
+					options={{
+						tabBarLabel: "Feed",
+						headerShown: false,
+						unmountOnBlur: true,
+					}}
+				/>
+				<Tabs.Screen
+					name='playlists'
+					options={{
+						tabBarLabel: "Playlists",
+						headerShown: false,
+						unmountOnBlur: true,
+					}}
+				/>
+				<Tabs.Screen
+					name='dico'
+					options={{
+						tabBarLabel: "Dico",
+						headerShown: false,
+						unmountOnBlur: true,
+					}}
+				/>
+				<Tabs.Screen
+					name='metiers'
+					options={{
+						tabBarLabel: "Metiers",
+						headerShown: false,
+						unmountOnBlur: true,
+					}}
+				/>
+				<Tabs.Screen
+					name='user'
+					options={{
+						tabBarLabel: "User",
+						headerShown: false,
+						href: null,
+					}}
+				/>
+				<Tabs.Screen
+					name='lesCitations'
+					options={{
+						tabBarLabel: "Les Citations",
+						headerShown: false,
+						unmountOnBlur: true,
+					}}
+				/>
+				<Tabs.Screen
+					name='secrets'
+					options={{
+						tabBarLabel: "3 Secrets",
+						headerShown: false,
+						href: null,
+					}}
+				/>
+				<Tabs.Screen
+					name='commandements'
+					options={{
+						tabBarLabel: "10 Commandements",
+						headerShown: false,
+						href: null,
+					}}
+				/>
+				<Tabs.Screen
+					name='petitesHistoires'
+					options={{
+						tabBarLabel: "Les Petites Histoires",
+						headerShown: false,
+						href: null,
+					}}
+				/>
+			</Tabs>
+		</TabBarVisibilityProvider>
 	);
 };
 
