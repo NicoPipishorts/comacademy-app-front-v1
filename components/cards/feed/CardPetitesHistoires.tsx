67ad9e7462b644c81ff02dcf83cd297a72e1a1e1
NicoPipishorts <87517338+NicoPipishorts@users.noreply.github.com>
2025-01-22 09:29:59 +0100

    Updated Feed videos visible only start

--- components/cards/feed/CardPetitesHistoires.tsx
+++ components/cards/feed/CardPetitesHistoires.tsx
@@ -1,24 +1,27 @@
-import { colorBlack } from "@/constants/colors";
-import { FontSize14, FontSizeH1 } from "@/constants/fontsizes";
 import { FeedItem } from "@/types/feed";
 import { ResizeMode, Video } from "expo-av";
+import React from "react";
 import { StyleSheet, View } from "react-native";
 
 interface Props {
 	data: FeedItem;
 	elementId: number;
+	visibleItems: number[];
 }
 
-export default function FeedCardPetitesHistoires({ data, elementId }: Props) {
+export default function FeedCardPetitesHistoires({
+	data,
+	visibleItems,
+}: Props) {
 	return (
 		<View style={styles.container}>
 			<Video
 				source={{
 					uri: `${process.env.EXPO_PUBLIC_URL}${data.payload.Media.url}`,
-				}} // Use the URI from the data
+				}}
 				style={{ width: 268, height: 476 }}
-				shouldPlay
-				isLooping={false} // Disable looping
+				shouldPlay={visibleItems.includes(data.id)}
+				isLooping={false}
 				useNativeControls={true}
 				resizeMode={ResizeMode.STRETCH}
 			/>
@@ -27,22 +30,5 @@ export default function FeedCardPetitesHistoires({ data, elementId }: Props) {
 }
 
 const styles = StyleSheet.create({
-	container: {
-		marginLeft: 10,
-		width: "84%",
-		minHeight: 100,
-		borderRadius: 25,
-		overflow: "hidden",
-	},
-	textTitle: {
-		color: colorBlack,
-		fontSize: FontSizeH1,
-		fontWeight: "bold",
-		marginBottom: 15,
-	},
-	textContent: {
-		color: colorBlack,
-		fontSize: FontSize14,
-		fontWeight: "bold",
-	},
+	container: { width: "86%", borderRadius: 10, overflow: "hidden" },
 });
