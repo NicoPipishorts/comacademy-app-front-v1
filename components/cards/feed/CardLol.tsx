b78c4d4a9b415674271372340307f64b2d494da1
NicoPipishorts <87517338+NicoPipishorts@users.noreply.github.com>
2025-07-24 17:25:11 +0200

    Update 1.1.2

--- components/cards/feed/CardLol.tsx
+++ components/cards/feed/CardLol.tsx
@@ -28,10 +28,7 @@ export default function FeedCardLol({ data, elementId }: Props) {
 	}, [data.payload.Media]);
 
 	// Build the image URL and ensure it has a fallback
-	const baseUrl = process.env.EXPO_PUBLIC_URL;
-	const imageUrl = data.payload.Media?.url
-		? `${baseUrl}${data.payload.Media.url}`
-		: null;
+	const imageUrl = data.payload.Media?.url ? data.payload.Media.url : null;
 	const source = imageUrl ? { uri: imageUrl } : null;
 
 	useEffect(() => {
