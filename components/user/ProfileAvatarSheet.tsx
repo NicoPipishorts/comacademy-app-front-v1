b07e763defe2873553be232128b8682d3611d522
NicoPipishorts <87517338+NicoPipishorts@users.noreply.github.com>
2026-03-04 10:26:37 +0100

    fixed deprecated media type

--- components/user/ProfileAvatarSheet.tsx
+++ components/user/ProfileAvatarSheet.tsx
@@ -156,7 +156,7 @@ export default function ProfileAvatarSheet({ visible, onClose }: Props) {
 			}
 
 			const pickResult = await ImagePicker.launchImageLibraryAsync({
-				mediaTypes: ImagePicker.MediaTypeOptions.Images,
+				mediaTypes: ["images"],
 				allowsEditing: true,
 				aspect: [1, 1],
 				quality: 0.8,
