075fa043f341afda31fcf3c5bc35df91b316d8a8
Nicolas Pisar <shamanproto@gmail.com>
2026-07-23 07:32:42 +0200

    fix: restore parcours reveal effects

--- components/leJeu/QuestionSwipeStack.tsx
+++ components/leJeu/QuestionSwipeStack.tsx
@@ -263,7 +263,7 @@ const styles = StyleSheet.create({
 	cardWrapper: {
 		position: "absolute",
 		width: "100%",
-		height: "88%",
+		height: "94%",
 		borderRadius: 24,
 		...Platform.select({
 			ios: {
